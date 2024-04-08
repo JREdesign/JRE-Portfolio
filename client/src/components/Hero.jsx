a533933f650a1f007efe1d40597150aadfae0670
JREdesign <jr.eugercios@gmail.com>
2024-04-08 15:20:49 +0200

    Cambios

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#e78009]' />
+          <div className='w-5 h-5 rounded-full bg-[#17e6ab]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
@@ -28,8 +28,8 @@ const Hero = () => {
       <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+        <a href='#about' style={{ display: 'flex', justifyContent: 'flex-start' }}>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-white-100 flex justify-center items-start p-2'>
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -39,7 +39,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className='w-3 h-3 rounded-full bg-white-100 mb-1'
             />
           </div>
         </a>

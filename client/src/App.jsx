a7a40b23851db43dd25bfee09c2f2c450ecb9bb7
JREdesign <jr.eugercios@gmail.com>
2024-04-08 19:10:02 +0200

    Avances

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0' style={{ backgroundColor: '#010107' }}>
+      <div className='relative z-0' style={{ backgroundColor: '#000003' }}>
 
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />

63a8c1b258958136dc0dcb5e36240b3855e05f07
JREdesign <jr.eugercios@gmail.com>
2024-04-11 20:04:40 +0200

    Contacto y cambio de escena

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -14,6 +14,7 @@ const Contact = () => {
     email: "",
     message: "",
   });
+  const [errors, setErrors] = useState({});
 
   const [loading, setLoading] = useState(false);
 
@@ -25,10 +26,35 @@ const Contact = () => {
       ...form,
       [name]: value,
     });
+
+    // Validación en tiempo real
+    validateField(name, value);
+  };
+
+  // Validación específica para cada campo
+  const validateField = (fieldName, value) => {
+    let tempErrors = { ...errors };
+
+    switch (fieldName) {
+      case 'name':
+        tempErrors.name = value.length > 30 || value.length === 0 ? "Por favor introduce un nombre válido" : "";
+        break;
+      case 'email':
+        tempErrors.email = !value.includes("@") || value.length === 0 ? "Por favor introduce un e-mail válido" : "";
+        break;
+      case 'message':
+        tempErrors.message = value.length < 10 ? "El mensaje debe contener mínimo 10 caracteres" : "";
+        break;
+      default:
+        break;
+    }
+
+    setErrors(tempErrors);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (Object.values(errors).some(error => error !== "")) return;
     setLoading(true);
 
     emailjs
@@ -54,6 +80,7 @@ const Contact = () => {
             email: "",
             message: "",
           });
+          setErrors({});
         },
         (error) => {
           setLoading(false);
@@ -65,9 +92,7 @@ const Contact = () => {
   };
 
   return (
-    <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
-    >
+    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className='flex-[0.75] bg-custom-blue p-8 rounded-2xl'
@@ -75,11 +100,7 @@ const Contact = () => {
         <p className={styles.sectionSubText}>Mándame un mensaje</p>
         <h3 className={styles.sectionHeadText}>Contacto</h3>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
-        >
+        <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Tu nombre</span>
             <input
@@ -90,6 +111,7 @@ const Contact = () => {
               placeholder="¿Cómo te llamas?"
               className='bg-custom-blue2 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
+            {errors.name && <p className="text-custom-orange">{errors.name}</p>}
           </label>
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Tu e-mail</span>
@@ -98,9 +120,10 @@ const Contact = () => {
               name='email'
               value={form.email}
               onChange={handleChange}
-              placeholder="¿Cual es tu dirección de correo electrónico?"
+              placeholder="¿Cuál es tu dirección de correo electrónico?"
               className='bg-custom-blue2 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
+            {errors.email && <p className="text-custom-orange">{errors.email}</p>}
           </label>
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Tu mensaje</span>
@@ -109,9 +132,10 @@ const Contact = () => {
               name='message'
               value={form.message}
               onChange={handleChange}
-              placeholder='¿Que és lo que quieres decir?'
+              placeholder='¿Qué es lo que quieres decir?'
               className='bg-custom-blue2 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
+            {errors.message && <p className="text-custom-orange">{errors.message}</p>}
           </label>
 
           <motion.button
--- client/src/components/canvas/Computers.jsx
+++ client/src/components/canvas/Computers.jsx
@@ -5,13 +5,13 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./desktop_pc/scene.glb");
 
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
-        position={[-20, 50, 10]}
+        position={[0, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
@@ -22,8 +22,8 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -0.5] : [0, -3.25, -0.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        position={isMobile ? [0, -4, -0.5] : [0, -1.25, -0.5]} // Modificación aquí para subir el objeto
+        rotation={[-0.01, 1, -0.1]} // Valores específicos para la orientación inicial
       />
     </mesh>
   );
@@ -33,21 +33,15 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };

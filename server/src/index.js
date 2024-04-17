e4071b95eb08ec591a4b2868f869abb3d6787170
JREdesign <jr.eugercios@gmail.com>
2024-04-17 18:56:38 +0200

    Securizacion variables entorno

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import { userRouter } from "./routes/user.js";
 import { experiencesRouter } from "./routes/experiences.js";
 import { techRouter } from "./routes/tech.js";
@@ -8,15 +9,20 @@ import { worksRouter } from "./routes/works.js";
 import { feedbacksRouter } from './routes/feedbacks.js';
 import servicesRouter from './routes/services.js';
 
+// Cargar variables de entorno
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// Endpoint de salud para comprobar que el servidor está funcionando
 app.get("/health", (req, res) => {
   res.status(200).send("Server is healthy!");
 });
 
-
+// Uso de routers para diferentes rutas
 app.use("/auth", userRouter);
 app.use("/services", servicesRouter);
 app.use("/experiences", experiencesRouter);
@@ -24,11 +30,11 @@ app.use("/tech", techRouter);
 app.use("/works", worksRouter);
 app.use("/feedbacks", feedbacksRouter);
 
-mongoose.connect(
-  "mongodb+srv://root:1234567890A@portfoliodb.nb2slxf.mongodb.net/portfoliodb?retryWrites=true&w=majority&appName=portfoliodb"
-)
+// Conexión a la base de datos utilizando una variable de entorno
+mongoose.connect(process.env.DB_CONNECTION_STRING)
 .then(() => console.log("Connected to MongoDB!"))
 .catch(err => console.error("Could not connect to MongoDB:", err));
 
-
-app.listen(3001, () => console.log("SERVER STARTED!"));
+// Iniciar el servidor en el puerto especificado en las variables de entorno
+const port = process.env.PORT || 3001; // Usar el puerto 3000 como predeterminado si PORT no está definido
+app.listen(port, () => console.log(`SERVER STARTED ON PORT ${port}!`));
